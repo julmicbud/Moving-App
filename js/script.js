f42228f6d74e525db97a6ecce54df0f2d25c174c
Julia Hendrickson <julmicbud@gmail.com>
2016-03-30 15:03:31 -0400

    feat: added New york times articles.

--- js/script.js
+++ js/script.js
@@ -20,6 +20,24 @@ function loadData() {
 		var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + ' ';
 		$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    var ntyimesUrl = 'http://api.nytimes.com/svc/search/vs/articlesearch.json?q' + cityStr + '&sort=newest&api-key=82266bdb0232a2bd8cc59aa83f3a55e9:13:74850154';
+    
+    $.getJSON(nytimesURL, function (data){
+    	// console.log(data);
+    	
+    	$nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    	
+    	articles = data.response.docs;
+    	for (var i=0; i<articles.length; i++){
+    		var article = articles[i];
+    		$nytElem.append('<li class="article">'+
+    		'<a href="'+article.web_url+'">'+article.headline.main+
+    				'</a>'+
+    				'<p>' + article.snippet + '</p>'+'</li>');
+    	};
+    	
+    });
+    
     return false;
 };
 
